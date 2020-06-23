cb25e9b6392239b3131cd13e4c150f8e110baeff
Sergey <sergey.migalnikov@novanet.no>
2020-06-23 09:09:19 +0200

    Test CI/CD using actions

--- Source/Tailwind.Traders.Web/Startup.cs
+++ Source/Tailwind.Traders.Web/Startup.cs
@@ -21,7 +21,7 @@ namespace Tailwind.Traders.Web
         public IConfiguration Configuration { get; }
         private ILogger Logger { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container .
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
